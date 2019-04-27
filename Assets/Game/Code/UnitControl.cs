6df9bad185280e6ddbd35f8821567563b4708a47
Brian Collins <collins.brian@gmail.com>
2019-04-27 14:21:54 -0700

    added character manager

--- Assets/Game/Code/UnitControl.cs
+++ Assets/Game/Code/UnitControl.cs
@@ -3,22 +3,80 @@ using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 
-public class UnitControl {
+public class UnitControl : MonoBehaviourPunCallbacks, IPunObservable {
+
+    public bool inHoldingArea = true;
+    MotionControl motionControl;
+    UnitInventory unitInventory;
 
     Weapon currentWeapon;
+    public Weapon CurrentWeapon {
+        get {
+            return currentWeapon;
+        }
+        set {
+            value.Equip(this);
+            currentWeapon.Stow();
+            currentWeapon = value;
+        }
+    }
+
+    public static GameObject LocalPlayerInstance;
+
+    public void Awake() {
+        gameObject.name = photonView.Owner.NickName;
+
+        if (photonView.IsMine) {
+            LocalPlayerInstance = gameObject;
+            StartCoroutine(MoveOutOfHoldingArea());
+        } else {
+
+            inHoldingArea = false;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
 
     void Start() {
-
+        motionControl = gameObject.AddComponent<MotionControl>();
+        unitInventory = gameObject.AddComponent<UnitInventory>();
     }
 
-    void Update() {
-
+    private IEnumerator MoveOutOfHoldingArea() {
+        yield return new WaitForSeconds(3);
+        inHoldingArea = false;
+        Transform bestSpawnLocation = SpawnPoint.FindSpawnLocation();
+        Debug.LogFormat("Moving {0} to {1}", transform.name, bestSpawnLocation.name);
+        transform.position = bestSpawnLocation.position;
     }
 
-    public bool EquipWeapon(Weapon newWeapon) {
-        currentWeapon = newWeapon;
-        currentWeapon.Equip(this);
-        return true;
+    public void SetInput(Vector2 keyInput, bool keyDown) {
+        motionControl.SetInput(keyInput, keyDown);
+    }
+
+    private void Update() {
+        if (!inHoldingArea) return;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+
+        if (stream.IsWriting) {
+            // We own this player: send the others our data
+            //stream.SendNext(this.IsFiring);
+            //stream.SendNext(this.Health);
+        } else {
+            // Network player, receive data
+            //this.IsFiring = (bool)stream.ReceiveNext();
+            //this.Health = (float)stream.ReceiveNext();
+        }
+    }
+ 
+    public int GetRounds(int amount) {
+        return amount;
+    }
+
+    public void AddWeapon(Weapon weapon) {
+        unitInventory.AddWeapon(weapon);
     }
 
 }
